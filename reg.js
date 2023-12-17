d34243a8c171338eb8664344cdbb94a35e23833c
nemorsell <dmitrijborisuk21@gmail.com>
2023-12-17 19:37:16 +0300

    возможность добавлять несколько карточек пользователя

--- reg.js
+++ reg.js
@@ -19,16 +19,18 @@ if(menuToggler){
 
 function sendForm(event){
     let error = {};
-    let address = event.target[6].value;
+    let address = event.target[5].value;
+    let addressTamplate = /[\w\W\d\D]{10,}/g;
+
     if(address.length < 10){
         error.address ="Слишком короткий адрес!";
         let addressField = document.getElementById("address-error");
         addressField.innerHTML = error.address;
-        address.previousElementSibling.classList.add("error");
+        addressField.previousElementSibling.classList.add("error");
     }
     else{
-        error.address = null;
-        let addressField = document.getElementById("adsress-error");
+        
+        let addressField = document.getElementById("address-error");
         addressField.innerHTML = '';
         addressField.previousElementSibling.classList.remove("error");
     }
@@ -41,10 +43,52 @@ function sendForm(event){
         nameField.previousElementSibling.classList.add("error");
     }
     else{
-    error.name = null;
+    
     let nameField = document.getElementById("name-error");
     nameField.innerHTML = '';
     nameField.previousElementSibling.classList.remove("error");
+    }
+
+4688268446826
+    let date = event.target[1].value;
+    let gender = event.target[2].value;
+    let email = event.target[3].value;
+    let url = event.target[4].value;
+    let errors = 0;
+    for(key in error){
+        errors++;
+
+    }
+
+    if(errors){
+        return false;
+    }
+    else{
+        let card = document.getElementsByClassName("card-list")[0];
+        let now = new Date();
+        let birthday = new Date(date);
+        let age = now.getFullYear() - birthday.getFullYear();
+    
+
+    card.insertAdjacentHTML("beforeend",
+        `<div class="card">
+        <img src="user.png">
+        <div class="card-name">${name}</div>
+        <div class="card-age">Возраст: ${age}</div>
+        <button class="">Инфо</button>
+        </div>`)
+    }
+    function startDrag(event){
+        event.dataTransfer.setData("text/html", event.target.id);
+        event.dataTransfer.effectAllowed = "move"
+    }
+    function enterDrag(event){
+        event.dataTransfer.dropEffect = "move"
+        event.target.classList.add("active");
+    }
+
+    function leaveDrag(event){
+        event.target.classList.remove("active");
     }
     return false;
 }
